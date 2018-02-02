195a37b0986c19e841f74dbf34984c0d552adfbf
André Schmith <schmith.andre@gmail.com>
2018-02-03 00:35:38 +0100

    small change

--- app/src/main/java/de/asschth/wasserstand/MainActivity.java
+++ app/src/main/java/de/asschth/wasserstand/MainActivity.java
@@ -2,6 +2,7 @@ package de.asschth.wasserstand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Button b = (Button) findViewById(R.id.welcheidauchimmerduvergiebst);
+
+        Log.d("LOG", "This is a log message");
     }
 }
