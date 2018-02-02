99d46503e49a7fdc092e91c186636279023b3fd5
André Schmith <schmith.andre@gmail.com>
2018-02-03 00:27:24 +0100

    Initial commit

---
+++ app/src/main/java/de/asschth/wasserstand/MainActivity.java
@@ -0,0 +1,15 @@
+package de.asschth.wasserstand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Button b = (Button) findViewById(R.id.welcheidauchimmerduvergiebst);
+    }
+}
